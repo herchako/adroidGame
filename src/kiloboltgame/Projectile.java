8908e1d66b261abfcf36f903781def3b42c291b3
herchako <hddk16@gmail.com>
2013-11-26 17:47:06 -0800

    DAY 3-8: What is Artificial Intelligence? Ultimo cap unidad 3

--- src/kiloboltgame/Projectile.java
+++ src/kiloboltgame/Projectile.java
@@ -6,40 +6,55 @@ public class Projectile {
 
 	private int x, y, speedX;
 	private boolean visible;
-	
+
 	private Rectangle r;
-	
-	public Projectile(int startX, int startY){
+
+	public Projectile(int startX, int startY) {
 		x = startX;
 		y = startY;
 		speedX = 7;
 		visible = true;
-		
+
 		r = new Rectangle(0, 0, 0, 0);
 	}
-	
-	public void update(){
+
+	public void update() {
 		x += speedX;
 		r.setBounds(x, y, 10, 5);
-		if (x > 800){
+		if (x > 800) {
 			visible = false;
 			r = null;
 		}
-		if (x < 800){
+		if (x < 800) {
 			checkCollision();
 		}
 	}
 
 	private void checkCollision() {
-		if(r.intersects(StartingClass.hb.r)){
+		if (r.intersects(StartingClass.hb.r)) {
 			visible = false;
-			StartingClass.score += 1;
-		}
-		
-		if (r.intersects(StartingClass.hb2.r)){
-			visible = false;
-			StartingClass.score += 1;
 
+			if (StartingClass.hb.health > 0) {
+				StartingClass.hb.health -= 1;
+			}
+			if (StartingClass.hb.health == 0) {
+				StartingClass.hb.setCenterX(-100);
+				StartingClass.score += 5;
+
+			}
+		}
+
+		if (r.intersects(StartingClass.hb2.r)) {
+			visible = false;
+
+			if (StartingClass.hb2.health > 0) {
+				StartingClass.hb2.health -= 1;
+			}
+			if (StartingClass.hb2.health == 0) {
+				StartingClass.hb2.setCenterX(-100);
+				StartingClass.score += 5;
+
+			}
 
 		}
 	}
@@ -75,6 +90,5 @@ public class Projectile {
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
-	
-	
+
 }
